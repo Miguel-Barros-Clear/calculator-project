5a892340b91c79307ab142f8cd2117e33f783d69
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-02 11:58:57 -0300

    feat: adding class attributes and methods

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,2 +1,22 @@
-class Controller{
+class Controller {
+    constructor() {
+        this._dataAtual;
+        this._displayCalc = "0";
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor; 
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
 }
