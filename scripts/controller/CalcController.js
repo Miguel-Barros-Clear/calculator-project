65c850c33bad13678029fa7ea4ba76d5ae775f7d
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-03 12:18:15 +0000

    feat: adding the calculate function

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -43,22 +43,48 @@ class CalcController {
         return this._operation[this._operation.length - 1] = value;
     }
 
-    isOperator(value){
+    isOperator(value) {
         return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+    }
+
+    pushOperation(value) {
+        this._operation.push(value)
+
+        if (this._operation.length > 3) {
+            this.calc();
+        }
+    }
+
+    calc(){
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+    }
+
+    setLastNumberToDisplay(){
+        
     }
 
     addOperation(value) {
         if (isNaN(this.getLastOperation())) {
-            if(this.isOperator(value)){
-                this._operation[this._operation.length - 1] = value;
-            }else if(isNaN(value)){
+            if (this.isOperator(value)) {
+                this.setLastOperation(value);
+            } else if (isNaN(value)) {
                 console.log(value);
             } else {
                 this._operation.push(value);
             }
         } else {
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
+
+            if (this.isOperator(value)) {
+                this._operation.push(value);
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+            }
         }
 
         console.log(this._operation);
