564e4d4f5723f28b8efd2dc9f8cbeb1884d8ad99
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-03 13:20:20 +0000

    feat: perform the last calculation using the calculate button

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,8 @@
 class CalcController {
     constructor() {
+        this._lastOperator = "";
+        this._lastNumber = "";
+
         this._operation = [];
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -58,14 +61,28 @@ class CalcController {
         }
     }
 
+    getResult() {
+        return eval(this._operation.join(""));
+    }
+
     calc() {
         let last = "";
 
-        if (this._operation.length > 3) {
-            last = this._operation.pop();
+        this._lastOperator = this.getLastItem();
+
+        if (this._operation.length < 3) {
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
         }
 
-        let result = eval(this._operation.join(""));
+        if (this._operation.length > 3) {
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+        } else if (this._operation.length == 3) {
+            this._lastNumber = this.getLastItem(false);
+        }
+
+        let result = this.getResult();
 
         if (last == '%') {
             result /= 100;
@@ -76,6 +93,23 @@ class CalcController {
         }
 
         this.setLastNumberToDisplay();
+    }
+
+    getLastItem(isOperator = true) {
+        let lastItem;
+
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                lastItem = this._operation[i];
+                break;
+            }
+        }
+
+        if (!lastItem) {
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+
+        return lastItem;
     }
 
     setLastNumberToDisplay() {
