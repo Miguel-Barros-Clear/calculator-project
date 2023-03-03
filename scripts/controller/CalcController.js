b67479073f115145e13daba60d02a633679c6823
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-03 13:56:41 +0000

    feat: adding sound to buttons

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor() {
+        this._audioOnOff = false;
+        this._audio = new Audio('click.mp3');
         this._lastOperator = "";
         this._lastNumber = "";
 
@@ -41,10 +43,29 @@ class CalcController {
             this.displayTime = this.currentDate.toLocaleTimeString(this._locate);
         }, 1000);
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio();
+            })
+        })
+    }
+
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio() {
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard() {
         document.addEventListener('keyup', e => {
+            this.playAudio();
 
             switch (e.key) {
                 case 'Escape':
@@ -235,6 +256,8 @@ class CalcController {
     }
 
     execBtn(value) {
+        this.playAudio();
+
         switch (value) {
             case "ac":
                 this.clearAll();
